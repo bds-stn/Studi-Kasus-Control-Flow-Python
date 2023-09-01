08365555502f490d80f8a9169dd306052940f864
bds-stn <69398615+bds-stn@users.noreply.github.com>
2023-09-01 10:50:37 +0700

    Update 04_menghitung_pengeluaran_harian.py

--- Nomer_4/04_menghitung_pengeluaran_harian.py
+++ Nomer_4/04_menghitung_pengeluaran_harian.py
@@ -1,3 +1,7 @@
+import locale
+
+locale.setlocale(locale.LC_ALL, "id_ID.UTF-8")
+
 pengeluaranSebulan = []
 
 for i in range(30):
@@ -10,5 +14,7 @@ for i in range(pengeluaranSebulan.__len__()):
 
 rata_rata_pengeluaran = totalPengeluaran / 30
 
-print(totalPengeluaran)
-print(rata_rata_pengeluaran)
+print(
+    f"\nTotal Pengeluaran Anda Sebesar: {locale.currency(totalPengeluaran, grouping=True)}")
+print(
+    f"Rata-Rata Pengeluaran Anda Sebesar: {locale.currency(rata_rata_pengeluaran, grouping=True)}\n")
